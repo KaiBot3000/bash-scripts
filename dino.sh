b7fa09c137ac51cba4762f3d0ded114f7b530ead
Kai <riselikethemoon@gmail.com>
2015-09-29 22:50:33 -0700

    added new arguments

--- dino.sh
+++ dino.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+PUNCH=""
 
 if [[ $1 = "danger!" ]]; then
 	STR="run, silly human!"
+elif [[ $1 = "danger" ]]; then
+	STR="Hmm, that sucks."
 elif [[ $1 = "kale" ]]; then
 	STR="An unsatisfying alternative."
+elif [[ $1 = "dna" ]]; then
+	STR="BINGO!"
+elif [[ $1 = "joke" ]]; then
+	STR="What's better than a talking dinosaur?"
+	PUNCH="A spelling bee!"
 else
 	STR="Hi!"
 fi
@@ -16,4 +24,6 @@ echo $STR
 printf "                           .-^^^-/ /  \n"
 printf "                        __/       /  \n"
 printf "                       <__.|_|-|_|  \n"
-printf "\n\n\n"
+printf "\n\n\t\t\t"
+echo $PUNCH
+printf "\n\n"
